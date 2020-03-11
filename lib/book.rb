d8e3af403658295b491bf733070a270da005af15
Ty Bandachit <tbandachit@gmail.com>
2020-03-11 22:10:13 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,24 @@
 class Book
   
+  attr_accessor :author, :page_count, :genre
   def initialize(title)
     @title = title
   end
   
   def title
     @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def page_count=(count)
+    @page_count = count
+  end
+  
+  def genre=(genre)
+    @genre = genre
   end
   
 end 
